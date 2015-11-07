cde90d1c4427a00893a938cab97fa2b472433c3b
alex <alex.m.larson@gmail.com>
2015-11-06 16:47:26 -0800

    changed styles, added blog page

---
+++ app/controllers/blog_controller.rb
@@ -0,0 +1,9 @@
+class BlogController < ApplicationController
+  def posts
+  end
+
+def inflation
+end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   get 'users/new'
 
-  get 'static_pages/about'
+get 'static_pages/about'
+
+  get 'blog/posts'
+  get 'blog/inflation'
+
+
+
 
   resources :comments do
     member do
